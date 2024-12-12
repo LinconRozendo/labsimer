127d9e688d0980cae7da8fae4b47712b3e8eed8b
LinconRozendo <lincon.silvar@cear.ufpb.br>
2024-12-12 11:08:36 -0300

    add pag infraestrutura

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -87,7 +87,7 @@ export default function App({ Component, pageProps, router}) {
       {isHomePage && (
             <DivBoxHomeAboutUs />
         )}
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
       <FooterBox />
     </div>
   )
--- src/pages/infraestrutura/index.js
+++ src/pages/infraestrutura/index.js
@@ -5,8 +5,24 @@ import styles from '@/styles/infraestrutura.module.css';
 
 export default function SobreNos() {
     return (
-        <div>
-            wooooooooowwwwwww
+        <div className={styles.divStructure}>
+            <h1 className={styles.textTitle}> 
+                INFRAESTRUTURA
+            </h1>
+
+            <h1 className={styles.textDescribe} >
+                O Laboratório de Simulação de Multifísica em Energia – LABSIMER está alocado na sala física de número 3.21, 
+                situada no 3º andar do novo prédio do CEAR.        
+            </h1>
+
+            <div className={styles.divImages}>
+                <div className={styles.images}>
+                    <img
+                        className={styles.imageLabSimer}
+                        src="/infraestrutura.jpg"
+                    />
+                </div>
+            </div>
         </div>
     )
 }
