42d9952270f996136918544e7e129ae7363e2668
LinconRozendo <lincon.silvar@cear.ufpb.br>
2024-12-12 10:48:34 -0300

    fixa footer no fim da página

---
+++ src/pages/infraestrutura/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '@/styles/infraestrutura.module.css';
+
+
+
+export default function SobreNos() {
+    return (
+        <div>
+            wooooooooowwwwwww
+        </div>
+    )
+}
